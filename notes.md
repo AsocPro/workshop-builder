729ad51225212879d219f07970f5f7edb1d08ce7
AsocPro <asocpro@gmail.com>
2024-07-23 20:28:02 -0600

    Add in progress postgresql deployments

---
+++ notes.md
@@ -0,0 +1,6 @@
+postgrest
+
+https://postgrest.org/en/v12/tutorials/tut1.html
+
+
+jwt: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidG9kb191c2VyIn0.7TPzLmz4OB4NFqvEefcAKOZKbUIIDF-y-beD2Q2peQg
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e
+
+
+NAMESPACE=management-console
+DOMAIN_NAME=test.test
+#DOMAIN_NAME=asoc.pro
+
+
+#kubectl create ns $NAMESPACE
+kubectl apply -n $NAMESPACE -f deployment_postgresql.yaml
+#kubectl create -n $NAMESPACE deployment wetty --image=wettyoss/wetty -- wetty --ssh-host=server
+kubectl apply -n $NAMESPACE -f deployment_postgrest.yaml
+kubectl expose -n $NAMESPACE deploy postgrest --port 3000
+kubectl expose -n $NAMESPACE deploy postgresql --port 5432
+
+
+
+
