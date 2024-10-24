d7c4ba6425329572608a84c619b309312d89df0e
AsocPro <asocpro@gmail.com>
2024-10-24 00:26:42 -0600

    Add in the datasource to grafana for loki

--- setup.sh
+++ setup.sh
@@ -32,3 +32,5 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install -n grafana --values loki-values.yaml loki grafana/loki
 
+kubectl apply -n grafana -f loki-grafana-datasource.yaml 
+
