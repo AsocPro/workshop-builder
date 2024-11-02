7b79bda933c795cd0d56e726cb64385779f8e823
AsocPro <asocpro@gmail.com>
2024-11-02 00:05:41 -0600

    Clean up postgres stuff as it was unused

--- setup.sh
+++ setup.sh
@@ -9,24 +9,10 @@ DOMAIN_NAME=test.test
 #DOMAIN_NAME=asoc.pro
 
 
-#kubectl create ns $NAMESPACE
-kubectl apply -n $NAMESPACE -f deployment_postgresql.yaml
-#kubectl create -n $NAMESPACE deployment wetty --image=wettyoss/wetty -- wetty --ssh-host=server
-kubectl apply -n $NAMESPACE -f deployment_postgrest.yaml
-kubectl expose -n $NAMESPACE deploy postgrest --port 3000
-kubectl expose -n $NAMESPACE deploy postgresql --port 5432
-
+kubectl create ns $NAMESPACE
 kubectl create -n $NAMESPACE service clusterip  pg-svc --tcp=5432:5432
 
 kubectl create -f https://github.com/grafana/grafana-operator/releases/latest/download/kustomize-cluster_scoped.yaml
-
-# Minio is used for loki operator. I just used the helm chart to make things quicker.
-#kubectl create namespace minio-tenant
-#
-#
-#kubectl apply -n $NAMESPACE -f minio-pv.yaml
-#
-#kubectl kustomize github.com/minio/operator\?ref=v6.0.3 | kubectl apply -f -
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
